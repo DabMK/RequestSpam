69f99ab643ce554316c814e2599f3099f6d4554e
DabMK <64862398+DabMK@users.noreply.github.com>
2024-06-16 11:59:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿namespace RequestSpam
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Dictionary<string, string> auth = new()
+            {
+                { "PHPSESSID", "" }
+            };
+
+            // LEVEL PASSWORD
+            string? pwd = null;
+            while (string.IsNullOrWhiteSpace(pwd))
+            {
+                Console.Write("\nPassword of level: ");
+                pwd = Console.ReadLine();
+            }
+
+            // PHP SESSION ID
+            string? ID = null;
+            while (string.IsNullOrWhiteSpace(ID))
+            {
+                Console.Write("PHP Session ID: ");
+                ID = Console.ReadLine();
+            }
+            auth["PHPSESSID"] = ID;
+
+            // DELAY
+            string? delayStr = string.Empty; int delay = 1000;
+            Console.Write("Delay between requests in ms (default is 1000): ");
+            delayStr = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(delayStr) && int.TryParse(delayStr, out _))
+            {
+                delay = int.Parse(delayStr);
+            }
+
+            // OUTPUT
+            string? output = string.Empty; bool records = false;
+            Console.Write("File where to output records (leave empty for no file output): ");
+            output = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(output) && Directory.Exists(Path.GetDirectoryName(output)))
+            {
+                if (File.Exists(output))
+                {
+                    Console.Write($"- The file \"{output}\" already exists. Do you want to override the content (Y/N)?");
+                    char selection = Console.ReadKey().KeyChar;
+                    if (selection == 'y')
+                    {
+                        File.WriteAllText(output, string.Empty);
+                        Console.Write($"- The file \"{output}\" will be overwritten");
+                        records = true;
+                    }
+                    else
+                    {
+                        Console.Write("- No file output will be generated");
+                        records = false;
+                    }
+                }
+                else
+                {
+                    records = true;
+                }
+            }
+            else
+            {
+                Console.Write("- The directory doesn't exist. No file output will be generated");
+            }
+
+            Console.WriteLine(Environment.NewLine);
+            int attempt = 1;
+            while (true)
+            {
+                HttpResponseMessage? response = Request.Get($"https://ae27ff.com/{pwd}.php", null, auth).GetAwaiter().GetResult();
+                string content;
+                if (response == null)
+                {
+                    content = "Error";
+                }
+                else
+                {
+                    content = response.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+
+                    if ((!content.Contains("<pre>") || !content.Contains("</pre>")) || content.Contains("couldn't be found"))
+                    {
+                        content = "Not Authenticated";
+                    }
+                    else
+                    {
+                        content = content.Split("<pre>")[1].Split("</pre>")[0];
+                        content = content.Substring(2, content.Length - 2);
+                    }
+                }
+
+                string record = $"ATTEMPT {attempt}, UTC TIME: {DateTime.UtcNow.ToString().Split(' ')[1]}{Environment.NewLine}{content}";
+                Console.WriteLine(record);
+                if (records)
+                {
+                    File.AppendAllText(output, record + Environment.NewLine);
+                }
+                Thread.Sleep(delay);
+                attempt++;
+            }
+        }
+    }
+}
---
+++ Request.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.ComponentModel;
+using System.Net;
+using System.Security.Authentication;
+using System.Text;
+using RequestSpam;
+
+namespace RequestSpam
+{
+    internal class Request
+    {
+        // SEND A "GET" WEB REQUEST
+        public static async Task<HttpResponseMessage?> Get(string? URI, Dictionary<string, string>? headers = null, Dictionary<string, string>? cookies = null)
+        {
+            HttpClient client;
+            HttpResponseMessage response = new();
+            HttpClientHandler handler = new();
+            CookieContainer container = new();
+            if (string.IsNullOrWhiteSpace(URI)) { return new(); }
+
+            // FORGING REQUEST
+            using (HttpRequestMessage request = new(new("GET"), URI))
+            {
+                // HEADERS
+                if (headers != null)
+                {
+                    for (int i = 0; i < headers.Count; i++)
+                    {
+                        request.Headers.Add(headers.Keys.ToList()[i], headers.Values.ToList()[i]);
+                    }
+                }
+
+                // COOKIES
+                if (cookies != null)
+                {
+                    handler.UseCookies = true;
+                    for (int i = 0; i < cookies.Count; i++)
+                    {
+                        container.Add(new Uri(URI), new Cookie(cookies.ElementAt(i).Key, cookies.ElementAt(i).Value));
+                    }
+                }
+                else
+                {
+                    handler.UseCookies = false;
+                }
+
+                // SEND
+                handler.CookieContainer = container;
+                client = new(handler, true);
+                try
+                {
+                    response = await client.SendAsync(request);
+                }
+                catch (Win32Exception e)
+                {
+                    Console.Write(e.Message);
+                    return null;
+                }
+                catch (Exception e)
+                {
+                    Console.Write(e.Message);
+                    return null;
+                }
+            }
+            return response;
+        }
+    }
+}
